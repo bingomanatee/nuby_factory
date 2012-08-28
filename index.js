9b22a3159ff15d7596cbee26942472420ceadaeb
Dave Edelhart <dave@wonderlandlabs.com>
2012-08-28 14:20:37 -0700

    Adding class specifically for creating functions, methods

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ var support = require('support');
 var Gate = support.Gate;
 var proper_path = support.proper_path;
 var elements = require('./lib/elements');
-
+var method_factory = require('./lib/method_factory');
 
 var Action = require('./lib/Action');
 var Controller = require('./lib/Controller');
@@ -17,24 +17,11 @@ var Component = require('./lib/Component');
 var Views_Folder = require("./lib/Views_Folder");
 var View_File = require("./lib/View_File");
 
-/* *********************** CONTROLLER ****************** */
-
-
-/* *************************** COMPONENT ******************** */
-
-/* **************************** ACTION ********************** */
-
-
-/* ***************************** VIEWS FOLDER ********************** */
-
-
-/* **************************** VIEW FILE ************************ */
-
-
 module.exports = {
     Action:Action,
     Component:Component,
     Controller:Controller,
     Views_Folder:Views_Folder,
-    View_File:View_File
+    View_File:View_File,
+    method_factory: method_factory
 }
